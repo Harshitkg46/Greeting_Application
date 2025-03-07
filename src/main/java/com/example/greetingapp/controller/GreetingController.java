b02eb9a478dd00bea1c6414c9dfd66977050042b
Harshit Goyal <harshitkg2003@gmail.com>
2025-03-07 11:56:46 +0530

    Added the HTTP methods

---
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.greetingapp.controller;
+
+import com.example.greetingapp.model.Greeting;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public Greeting getGreeting() {
+        return new Greeting("Hello, this is a GET request!");
+    }
+
+    @PostMapping
+    public Greeting postGreeting(@RequestBody Greeting greeting) {
+        return new Greeting("Received POST request with message: " + greeting.getMessage());
+    }
+
+    @PutMapping
+    public Greeting putGreeting(@RequestBody Greeting greeting) {
+        return new Greeting("Received PUT request with message: " + greeting.getMessage());
+    }
+}
